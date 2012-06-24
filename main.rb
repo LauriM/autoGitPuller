1852fa7c5facb0226b5363bb88f2c8bf3aa23b90
Lauri Makinen <lauri.sakari.makinen@gmail.com>
2012-06-24 13:16:46 +0300

    Added bitbucket support

--- main.rb
+++ main.rb
@@ -24,14 +24,42 @@ def backupGithub(username,password)
 		directory = $config["directory"] + "github/" + username + "/" + data[i]["name"]
 
 		if !File.directory? directory
-			# Directory doesn't exists yet, create it
 			system("mkdir -p " + directory)
 
-			# Do a starting clone
 			system("git clone " + data[i]["ssh_url"] + " " + directory)
 		else
-			# Directory has already been created pull to it
-			system("cd " + directory + " && git pull origin master")
+			system("cd " + directory + " && git pull")
+		end
+		i += 1
+	end
+end
+
+def backupBitbucket(username,password)
+	puts "Backupping Bitbucket/" + username
+	command = "curl -s -u " + username + ":"+ password +" curl https://api.bitbucket.org/1.0/users/" + username  + "/"
+	output  = IO.popen(command)
+	output  = output.readlines
+	output  = output.join()
+	data = JSON.parse(output)
+
+	i = 0
+
+	while i < data["repositories"].length do
+		puts "Bitbucket/"+username+"/"+data["repositories"][i]["name"]
+
+		directory = $config["directory"] + "bitbucket" + username + "/" + data["repositories"][i]["name"]
+		git_url   = "git@bitbucket.org:" + username + "/" + data["repositories"][i]["name"] + ".git"
+
+		if data["repositories"][i]["scm"] == "git"
+
+			if !File.directory? directory
+				system("mkdir -p " + directory)
+
+				system("git clone " + git_url + " " + directory) 
+			else
+				system("cd " + directory + " && git pull")
+			end
+
 		end
 		i += 1
 	end
@@ -41,8 +69,12 @@ i = 0
 
 while i < $config["repos"].length do
 
-	if $config["repos"][i]["type"] == "Github"
+	if $config["repos"][i]["type"].downcase == "github"
 		backupGithub($config["repos"][i]["username"],$config["repos"][i]["password"])
+	end
+
+	if $config["repos"][i]["type"].downcase == "bitbucket"
+		backupBitbucket($config["repos"][i]["username"],$config["repos"][i]["password"])
 	end
 
 	i += 1
